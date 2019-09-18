e0a922e2ee068709c867c019f52421942d3a17cc
Ruth Baker <ruthmoog@gmail.com>
2019-09-18 08:40:46 +0100

    Refactor out long ternary in twofer exercise

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,5 +1,6 @@
 class Twofer {
     String twofer(String name) {
-      return name == null ? "One for you, one for me." : "One for " + name + ", one for me.";
+      String whoItsFor = name == null ? name = "you" : name;
+      return "One for " + whoItsFor + ", one for me.";
     }
 }
